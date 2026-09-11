aabaaa8ba622740127116ac1b2a0266fc7836094
Leivyhan <25-15184@g.batstate-u.edu.ph>
2026-09-11 11:54:43 +0800

    Delete act2_temp.py

--- act2_temp.py
+++
@@ -1,5 +0,0 @@
-celsius = float(input("Enter temperature in degree C:"))
-farenheit = celsius * 9/5 + 32
-is_between = 20 <= celsius <= 30
-print("Farenheit:", farenheit)
-print("Between 20 and 30 degree C:", is_between)
